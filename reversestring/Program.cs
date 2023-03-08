435d928669ed181877bd1a54e1e631345f39755d
Beccapf <beccafariasilva@gmail.com>
2023-03-08 17:03:49 -0300

    String reverse c#

---
+++ reversestring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace reversestring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len;
+            string str, revstr = "";
+            Console.Write("Digite uma string:");
+            str = Console.ReadLine();
+
+            len = str.Length - 1;
+            while (len >= 0)
+            {
+                revstr = revstr + str[len];
+                len--;
+            }
+            Console.WriteLine("String invertida:" + revstr);
+        }
+    }
+}
